516a3ee887111904f990db5faba103d3094fb12f
Muhammad Shahzad <k242008@thekit.edu.pk>
2026-08-04 12:00:58 +0500

    Create FastAPI application

---
+++ backend/app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "DigiEmp"
+    VERSION: str = "1.0.0"
+    DEBUG: bool = True
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+
+settings = Settings()
